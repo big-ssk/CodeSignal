16d4687cfaa8ae8b3f60a856398d56ec015d0d28
big-ssk <67604974+big-ssk@users.noreply.github.com>
2020-11-10 18:16:40 +0300

    Create 25_array_replace.py

---
+++ intro/25_array_replace.py
@@ -0,0 +1,4 @@
+# https://app.codesignal.com/arcade/intro/level-6/mCkmbxdMsMTjBc3Bm
+
+def arrayReplace(inputArray, elemToReplace, substitutionElem):
+    return [x if x != elemToReplace else substitutionElem for x in inputArray]
