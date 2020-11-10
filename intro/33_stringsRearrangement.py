8cf1c112ccfbf3f7b64eb5e75344b929f4546429
big-ssk <67604974+big-ssk@users.noreply.github.com>
2020-11-10 18:29:56 +0300

    Create 33_stringsRearrangement.py

---
+++ intro/33_stringsRearrangement.py
@@ -0,0 +1,26 @@
+import itertools
+def stringsRearrangement(input_array):
+    for perm in itertools.permutations(input_array):
+        if _consecutive_strings_differ_by_one_character(perm):
+            return True
+    return False
+
+
+def _consecutive_strings_differ_by_one_character(input_array):
+    iter_input_array = input_array[:-1]
+    for i, current_str in enumerate(iter_input_array):
+        next_str = input_array[i + 1]
+        if not _differ_by_one_char(current_str, next_str):
+            return False
+    return True
+
+
+def _differ_by_one_char(str1, str2):
+    result = False
+    for char1, char2 in zip(str1, str2):
+        if char1 != char2:
+            if result:
+                return False
+            else:
+                result = True
+    return result
