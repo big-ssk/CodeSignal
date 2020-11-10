4606e9830376578a6308071a09a020414b952c48
big-ssk <67604974+big-ssk@users.noreply.github.com>
2020-11-10 18:51:08 +0300

    Create 59_spiralNumbers.py

---
+++ intro/59_spiralNumbers.py
@@ -0,0 +1,10 @@
+def spiralNumbers(n):
+    m = [[0] * n for _ in range(n)]
+    i, j, di, dj = 0, 0, 0, 1
+
+    for k in range(n * n):
+        m[i][j] = k + 1
+        if (not -1 < i + di < n) or (not -1 < j + dj < n) or m[i + di][j + dj] != 0:
+            di, dj = dj, -di
+        i, j = i + di, j + dj
+    return m
